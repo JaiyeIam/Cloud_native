6a22066dd7dcd617df9d8312568d2cec64b3fbcc
Jaiyee.iam <106023345+JaiyeIam@users.noreply.github.com>
2024-06-01 17:03:56 +0000

    after debug pipeline test to gitlab

--- shopping-cart/server.js
+++ shopping-cart/server.js
@@ -20,4 +20,4 @@ app.get('/', function (req, res) {
 app.listen(3002, function () {
   console.log("app listening on port 3002!");
 });
-// and finally you the latest file to be productif 
+// and finally you the latest file to be productif et gagner du temps 
