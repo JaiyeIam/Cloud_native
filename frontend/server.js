4811fda80a45c9e95851754f2641892558877b18
Jaiyee.iam <106023345+JaiyeIam@users.noreply.github.com>
2024-06-01 14:02:29 +0000

    coder comme un fou

--- frontend/server.js
+++ frontend/server.js
@@ -71,4 +71,4 @@ app.get('/get-shopping-cart', function (req, res) {
 app.listen(3000, function () {
   console.log("app listening on port 3000!");
 });
-// this is another way to write code 
+// this is another way to write code comme un fou 
